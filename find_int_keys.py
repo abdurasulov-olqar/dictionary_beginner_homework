a2ebed58d40f4386fc06282cf935ac867b4eee43
olqarabdurasulov <abdurasulovolqar123@mail.com>
2023-02-13 21:45:12 +0500

    key si int bo`lganlar

--- find_int_keys.py
+++ find_int_keys.py
@@ -6,4 +6,15 @@ def find_int_keys(data: dict) -> list:
     Returns:
         list: A list of all keys in the dictionary that are integers.
     """
-    return 
+    l = []
+    for i in data.keys():
+        if type(i) == int:
+            l.append(i)
+    return l
+data = {
+    'a': 1, 
+    3 : 2, 
+    'c': 3,
+    10:'a'
+  }
+print(find_int_keys(data))
